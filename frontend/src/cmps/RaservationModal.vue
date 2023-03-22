018304ca44b6691c594e1a76f5f04094244180ba
Nadav Dori <dori.nadav@gmail.com>
2023-03-22 23:37:54 +0200

    modal reservation now stiky

--- frontend/src/cmps/RaservationModal.vue
+++ frontend/src/cmps/RaservationModal.vue
@@ -1,48 +1,62 @@
 <template>
-<section class="reserve-moadl">
-          <form @submit="reserve">
-     
-    <div class="order-form-header">
-      <p><span class="cost">$150</span> / night</p>
-      <p>4.38 <span class="reviews">(4 reviews)</span></p>
-    </div>
-    <div class="order-data">
-      <div class="date-picker">
-        <div class="date-input">
-          <label>CHECK IN</label>
-          <input value="Tue Sep 07 2021">
+  <section class="reserve-moadl">
+    <form @submit="reserve">
+      <div class="order-form-header">
+        <p><span class="cost">$150</span> / night</p>
+        <p>4.38 <span class="reviews">(4 reviews)</span></p>
+      </div>
+      <div class="order-data">
+        <div class="date-picker">
+          <div class="date-input">
+            <label>CHECK IN</label>
+            <input value="Tue Sep 07 2021" />
+          </div>
+          <div class="date-input">
+            <label>CHECK OUT</label>
+            <input value="Tue Sep 07 2021" />
+          </div>
         </div>
-        <div class="date-input">
-          <label>CHECK OUT</label>
-          <input value="Tue Sep 07 2021">
+
+        <div class="guest-input">
+          <label>GUESTS</label>
+          <input value="2" />
+          <svg
+            class="angle-down-svg"
+            viewBox="0 0 320 512"
+            width="100"
+            title="angle-down"
+          >
+            <path
+              d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"
+            />
+          </svg>
         </div>
       </div>
-
-      <div class="guest-input">
-        <label>GUESTS</label>
-        <input value="2">
-        <svg class="angle-down-svg" viewBox="0 0 320 512" width="100" title="angle-down">
-          <path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z" />
-        </svg>
-      </div>
-    </div>
-
-            <div class="date-picker">date picker</div>
-            <ReserveBtn />
-          </form>
-        </section>
+      <ReserveBtn />
+    </form>
+    <p class="details-disclamer">You won't be charged yet</p>
+  <div class="prices">
+    <p class="under-line">$555 X 4 nights</p>
+    <p class="text-align-right">2,380</p>
+    <p class="under-line" >Cleaning fee </p>
+    <p class="text-align-right">$238</p>
+    <p class="under-line">Sercives fee </p>
+    <p class="text-align-right">$400.6</p>
+  </div>
+  <div class="total">
+        <p>Total </p>
+    <p>$3,022.60</p>
+  </div>
+  </section>
 </template>
 <script>
 import ReserveBtn from "../cmps/ReserveBtn.vue";
 export default {
-    data(){
-        return {
-
-        }
-    },
- components: {
-      ReserveBtn,
-    
-  }
-}
+  data() {
+    return {};
+  },
+  components: {
+    ReserveBtn,
+  },
+};
 </script>
