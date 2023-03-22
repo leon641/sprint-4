114cf9dc7789a96ea72214dc50dce6c83c85a5b9
Shimon Katsman <shkatsman@gmail.com>
2023-03-22 16:24:11 +0200

    added some style to filter

--- frontend/src/cmps/Filter.vue
+++ frontend/src/cmps/Filter.vue
@@ -1,19 +1,38 @@
 <!-- בס"ד -->
 
 <template>
-  <section class="filter">
-    <button>
-      <div>Anywhere</div>
-    </button>
-    <button>
-      <div>Any week</div>
-    </button>
-    <button>
-      <div>Add guests
-      <div className="icon" v-html="getSvg('search')"></div>
-      </div>
-    </button>
-  </section>
+  <div class="filter">
+    <section class="filter-btns">
+      <button>
+        <div>Anywhere</div>
+      </button>
+      <div class="separator"></div>
+      <button>
+        <div>Any week</div>
+      </button>
+      <div class="separator"></div>
+      <button>
+        <div>
+          Add guests
+          <div className="icon">
+            <svg
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+            >
+              <g fill="none">
+                <path
+                  d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"
+                ></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </button>
+    </section>
+  </div>
 </template>
 
 <script>
