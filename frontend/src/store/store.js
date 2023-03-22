0b53fa5983ba8fb06f48e53a735803869bee105a
Shimon Katsman <shkatsman@gmail.com>
2023-03-22 19:58:28 +0200

    header in work

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,16 +2,16 @@
 
 import Vuex from 'vuex'
 
-import { userStore } from './user.store.js'
+// import { userStore } from './user.store.js'
 import { stayStore } from './stay.store.js'
-import { reviewStore } from './review.store.js'
+// import { reviewStore } from './review.store.js'
 
 export const store = Vuex.createStore({
   strict: true,
   modules: {
-    userStore,
+    // userStore,
     stayStore,
-    reviewStore
+    // reviewStore
   },
   state: {
   },
