c542060ab846e276b4fd638153bad068056ec1c6
Shimon Katsman <shkatsman@gmail.com>
2023-03-24 16:50:27 +0300

    regions modal layout done

--- frontend/src/cmps/FilterModal.vue
+++ frontend/src/cmps/FilterModal.vue
@@ -1,7 +1,22 @@
 <!-- בס"ד -->
 
 <template>
-  <div v-if="propWhere" class="where-modal"></div>
+  <div v-if="propWhere" class="where-modal">
+    <div class="flex">
+      <div class="resent-searches">
+        <h5>Recent searches</h5>
+      </div>
+      <div class="regions">
+        <h5>Search by region</h5>
+        <div class="grid">
+          <div v-for="region in regions" :key="region">
+            <img :src="region.url" />
+            <p>{{ region.title }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div v-if="propCheck" class="check-modal"></div>
   <div v-if="propWho" class="who-modal"></div>
 </template>
@@ -14,7 +29,34 @@ export default {
     propWho: String,
   },
   data() {
-    return {};
+    return {
+      regions: [
+        {
+          title: "I'm flexible",
+          url: "@/assets/img/regions/I'm flexible.jpg",
+        },
+        {
+          title: "Italy",
+          url: "@/assets/img/regions/Italy.webp",
+        },
+        {
+          title: "Middle East",
+          url: "@/assets/img/regions/Middle East.webp",
+        },
+        {
+          title: "France",
+          url: "@/assets/img/regions/France.jpg",
+        },
+        {
+          title: "South America",
+          url: "@/assets/img/regions/South America.jpg",
+        },
+        {
+          title: "United Kingdom",
+          url: "@/assets/img/regions/United Kingdom.jpg",
+        },
+      ],
+    };
   },
   computed: {},
   methods: {},
