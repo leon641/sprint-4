7d962eb791d408142b44940fd2049366ae1b2d5e
leon641 <leonshoushan54@gmail.com>
2023-03-26 13:21:29 +0300

    fix imgs

--- frontend/src/services/stay.service.local.js
+++ frontend/src/services/stay.service.local.js
@@ -106,8 +106,7 @@ function getEmptyOrder() {
       "imgUrls": ["https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
       "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
       "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
-      "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
-      "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large"
+    
       
     ],
       "price": 80.00,
