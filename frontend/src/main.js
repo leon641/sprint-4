a1deb93b01b4385eab1023b45cc89b249a705d23
Nadav Dori <dori.nadav@gmail.com>
2023-03-27 09:57:34 +0300

    broght calender

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 
 import { router } from './router.js'
 import { store } from './store/store.js'
-
+import VCalendar from 'v-calendar'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import 'v-calendar/style.css';
 import './assets/styles/main.scss'
 import App from './App.vue'
 
@@ -15,5 +15,5 @@ const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
 app.use(store)
-
+app.use(VCalendar, {})
 app.mount('#app')
