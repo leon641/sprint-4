13f524d15dc3415b7cb1869d568b7e10eb157fe2
leon641 <leonshoushan54@gmail.com>
2023-03-29 00:21:22 +0300

    stayEdit in progress

--- frontend/src/views/StayEdit.vue
+++ frontend/src/views/StayEdit.vue
@@ -2,6 +2,20 @@
 
 <template>
   <h1>hollllaaaa</h1>
+  <section class="stay-edit">
+    <h3>Name</h3>
+    <form @submit.prevent="submitStay">
+    <input class="add-input-name" type="text" placeholder="Name">
+    <h3>Address</h3>
+    <div class="address-inputs">
+    <input class="add-input" type="text" placeholder="City">
+    <input class="add-input" type="text" placeholder="Country">
+    <input class="add-input" type="text" placeholder="Street">
+    </div>
+    <button class="save-form-btn">save</button>
+
+    </form>
+  </section>
 </template>
 <script>
 export default {
