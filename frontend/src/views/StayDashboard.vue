adf2f9d136f0bae12cc9930c5744401ffd054755
Nadav Dori <dori.nadav@gmail.com>
2023-03-28 20:42:27 +0300

    add dashboard page

---
+++ frontend/src/views/StayDashboard.vue
@@ -0,0 +1,21 @@
+<template>
+<section class="dashBoard-container">
+    
+</section>
+<h1>hiiiiiiiiii</h1>
+</template>
+<script>
+export default {
+    created(){
+        this.loggedinUser= this.$store.getters.loggedinUser
+        console.log('loging in dash',this.loggedinUser);
+        
+    },
+  data() {
+    return {
+        loggedinUser:null
+    };
+  },
+  computed: {},
+};
+</script>
