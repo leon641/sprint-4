11cf968a7f7d3caaf7b3b308383bd6d7fae1a530
Nadav Dori <dori.nadav@gmail.com>
2023-03-29 21:19:38 +0300

    added footer

--- frontend/src/cmps/AppFooter.vue
+++ frontend/src/cmps/AppFooter.vue
@@ -1,8 +1,17 @@
 <!-- בס"ד -->
 
 <template>
-<section>
-  
+<section class="main-footer">
+  <div class="footer-items">
+
+  <span class="footer-item item1">© 2023 Appbnb, Inc.</span>
+  <span>·</span>
+  <span class="footer-item item2"> Terms</span>
+  <span>·</span>
+  <span class="footer-item item3">Sitemap</span>
+  <span>·</span>
+  <span class="footer-item item4">Privacy</span>
+  </div>
 </section>
 </template>
 <script>
