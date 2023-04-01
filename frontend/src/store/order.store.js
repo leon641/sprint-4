76af81fe1d54ecf17f75f6c62e98e23e2f5c135a
leon641 <leonshoushan54@gmail.com>
2023-04-01 23:06:31 +0300

    user trip

--- frontend/src/store/order.store.js
+++ frontend/src/store/order.store.js
@@ -43,7 +43,7 @@ export const orderStore = {
 
     mutations: {
         setOrders(state, { orders }) {
-            state.orders = orders
+            state.orders.push(orders)
         },
         setCurrOrder(state, { order }) {
             state.currOrder = order
@@ -64,6 +64,7 @@ export const orderStore = {
         async setOrder({ commit }, { order }) {
 
             await orderService.setOrder(order)
+            commit({ type: 'setOrders', order })
           
         },
         async setCurrOrder({ commit }, { order }) {
