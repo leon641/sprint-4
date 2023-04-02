16fe0a8c6105ea40e0031ca9d2da208fac354203
leon641 <leonshoushan54@gmail.com>
2023-04-02 12:15:39 +0300

    chart

---
+++ frontend/src/cmps/BarChart.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <BarChart :chartData="testData" :options="options"/>
+</template>
+
+<script lang="ts">
+import { BarChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
+export default {
+    name: 'stay chart',
+    components: { BarChart },
+    data() {
+        return {
+            testData: {
+                labels: ['June', 'July', 'August', 'September', 'October', 'November '],
+                datasets: [
+                    {
+                        data: [100, 130, 40, 60, 70, 20],
+                        backgroundColor: [
+                            'darkseagreen',
+                            'orange',
+                            '#0079AF',
+                            '#123E6B',
+                            '#8a4fe8',
+                            '#A5C8ED',
+                        ],
+                    },
+                ],
+            },
+            options: {
+                plugins: {
+                    legend: { display: false }
+                    // legend: { position: 'left' }
+                }, 
+                methods : {
+
+                },
+                computed : {
+  
+   
+                },
+            }
+        }
+    },
+}
+</script>
